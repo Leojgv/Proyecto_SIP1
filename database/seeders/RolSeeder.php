2056958a08dda6289f7d416ff1be50cc0599e6d1
Leonardo Gonzalez <>
2025-11-08 00:37:12 -0300

    seeders

---
+++ database/seeders/RolSeeder.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+namespace Database\Seeders;
+
+use App\Models\Rol;
+use Illuminate\Database\Seeder;
+
+class RolSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $roles = [
+            ['nombre' => 'Admin', 'descripcion' => 'Administrador del sistema'],
+            ['nombre' => 'Estudiante', 'descripcion' => 'Usuario estudiante'],
+            ['nombre' => 'Asesor', 'descripcion' => 'Asesor pedagógico'],
+            ['nombre' => 'Docente', 'descripcion' => 'Profesor de asignaturas'],
+            ['nombre' => 'Director de Carrera', 'descripcion' => 'Director responsable de la carrera'],
+        ];
+
+        foreach ($roles as $rol) {
+            Rol::updateOrCreate(
+                ['nombre' => $rol['nombre']],
+                ['descripcion' => $rol['descripcion']]
+            );
+        }
+    }
+}
