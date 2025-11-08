4e046ce874cac92cd748171e7c7b5149b6929226
Leonardo Gonzalez <>
2025-11-08 00:36:02 -0300

    others

---
+++ app/Notifications/DashboardNotification.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class DashboardNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(
+        public string $titulo,
+        public string $mensaje,
+        public ?string $url = null,
+        public ?string $textoBoton = null
+    ) {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'titulo' => $this->titulo,
+            'mensaje' => $this->mensaje,
+            'url' => $this->url,
+            'texto_boton' => $this->textoBoton,
+        ];
+    }
+}
