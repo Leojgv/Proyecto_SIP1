357ea98407d94472a41b4a734a2b5eda85297f47
Leonardo Gonzalez <>
2025-11-12 23:55:20 -0300

    Agregar soporte para Bootstrap 5 en la paginación

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,5 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        Paginator::useBootstrapFive();
     }
 }
