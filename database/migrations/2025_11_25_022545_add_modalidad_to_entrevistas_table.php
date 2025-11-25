16b7858c0ac4ca10dbafcaf2d6cfff6fd2230957
Leonardo Gonzalez <>
2025-11-25 02:10:09 -0300

    feat: Add migrations for 'titulo' field in Solicitudes and 'modalidad' field in Entrevistas tables
    - Created a migration to add a nullable 'titulo' field to the Solicitudes table for better request identification.
    - Introduced a migration to add a nullable 'modalidad' field to the Entrevistas table, allowing for specification of interview format.

---
+++ database/migrations/2025_11_25_022545_add_modalidad_to_entrevistas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('entrevistas', function (Blueprint $table) {
+            $table->enum('modalidad', ['Virtual', 'Presencial'])->nullable()->after('fecha_hora_fin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('entrevistas', function (Blueprint $table) {
+            $table->dropColumn('modalidad');
+        });
+    }
+};
